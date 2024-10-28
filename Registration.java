4a8d2729e5d80747627b06d7d1a7a7e9d716ed9f
FYFYBai <qihe2000@foxmail.com>
2024-10-27 20:26:21 -0400

    enroll new students method
    updated enrollNewStudent method

--- Registration.java
+++ Registration.java
@@ -11,17 +11,23 @@ package CourseRegistrationSystem;
 public class Registration {
 
     //This method take an string array that include Student Name, Student Number
-    //and an string array that include course number, number of spots, course name and teacher
-    //and returns a boolean indicating if registration is available
-    public boolean enrollNewStudent(String[] student, String[] course){
-        int spotsAvailable = Integer.valueOf(course[1]) ;
+    //and an string array that include course number, course name, professor, max amount of seats, students enrolled
+    //and updates the course info if registration suceeded
+    public void enrollNewStudent(String[] student, String[] course){
+        //initialize variables for future use in the method
+        int studentEnrolled = Integer.valueOf(course[4]);
+        int maxSeat = Integer.valueOf(course[3]);
+        int spotsAvailable = maxSeat - studentEnrolled;
+
+        //case if there are enough spots
         if (spotsAvailable > 0) {
-            System.out.printf("Your Registration has been complete and recorded for %s!%n", course[2]);
-            return true;
+            System.out.printf("Your Registration has been complete and recorded for %s!%n", course[1]);
+            course[4] = String.valueOf(studentEnrolled += 1);
         }
+
+        //case if there are not enough spots
         else{
-            System.out.printf("There are not enough seats available in course %s! Registration Failed!%n", course[2]);
-            return false;
+            System.out.printf("There are not enough seats available in course %s! Registration Failed!%n", course[1]);
         }
     }
 
