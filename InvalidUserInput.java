897cb1f0d063ad274cfa8e7162727c7aebcbdc76
FYFYBai <qihe2000@foxmail.com>
2024-10-30 10:02:34 -0400

    Update InvalidUserInput.java

--- InvalidUserInput.java
+++ InvalidUserInput.java
@@ -3,10 +3,13 @@ package CourseRegistrationSystem;
 //Defining custom Exception that is thrown when invalid choice is given by user
 
 public class InvalidUserInput extends RuntimeException{
+
+    //allowing custom message
     public InvalidUserInput(String message) {
         super(message);        
     }
 
+    //supports exception throw
     public InvalidUserInput(String message, Throwable cause) {
         super(message, cause);
     }
