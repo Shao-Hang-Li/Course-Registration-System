022c5015e46800163eac604f1d19b7e4dbfcd6b2
SDXtreme <oshawanick@yahoo.ca>
2024-10-30 00:19:47 -0400

    Added javadoc
    Javadoc has been added and should be working properly.

--- Student.java
+++ Student.java
@@ -1,29 +1,56 @@
 package CourseRegistrationSystem;
-
+/** 
+ * The Student class reprents the student names and IDs with the ability to set new names and assign them new ids.
+ * 
+ * <p>This class keeps track of all the students that have or will register to the school. It is recorded with their names and their ids.
+ * 
+ * 
+ * @see Course
+ * 
+ */ 
 public class Student {
-    // Variables and types declaration
+    //Variables for student ID and Student name
+    /**
+     * The student's ID
+     */
     private final int studentID;                // Student ID number (Unique)
+    /**
+     * The student's name
+     */
     private final String studentName;      // Student name
 
-    // Constructor for Student (with their respective parameters)
+    /**
+     * Allows the student name and id to be set.
+     * @param studentName
+     * @param studentID
+     */
     public Student(String studentName, int studentID) {
         this.studentName = studentName;   // Set the student name
         this.studentID = studentID;                 // Set the student ID
     }
 
-    // Getter for the student first name
+    /**
+     * shows Student Name
+     * @return
+     */
     public String getStudentName() {
         return studentName;
 
     }
 
-    // Getter for the student ID
+    /**
+     * Shows student ID
+     * @return
+     */
     public int getStudentID() {
         return studentID;
     }
 
 
-    // Display the student information by returning a string (studentID, studentFirstName, studentLastName)
+    /**
+     * Displays full infor for student ID and student Name
+     * @return
+     */
     public String displayStudentInfo() {
         return "Student ID: " + studentID + ", Name: " + studentName;
     }
